dfa087da8f9b8f3b263191a048766b51509ec1c4
itty.todder <itty.todder@gmail.com>
2022-03-22 23:41:30 +0200

    Prettified(a bit) Generator provider

--- CharacterGenerator/GeneratorProvider.py
+++ CharacterGenerator/GeneratorProvider.py
@@ -20,12 +20,16 @@ class GeneratorProvider:
         races_root_node = generator_xml_file_root.find(RACES_XML_NODE)
         races_nodes = races_root_node.findall(RACE_XML_NODE)
         self.races = []
-        #TODO: Prettify(select only value)
         for race in races_nodes:
-            sub_races_nodes = race.findall(SUB_RACE_XML_NODE)
-            sub_races = []
-            if sub_races_nodes is not None:
-                for sub_race in sub_races_nodes:
-                    sub_races.append(GeneratedItem(sub_race.get("Name")))
-            self.races.append(GeneratedItem(race.get("Name"), sub_races))
+            generated_race = self.__generate_item_from_xml_node(race, SUB_RACE_XML_NODE)
 
+            self.races.append(generated_race)
+
+    @staticmethod
+    def __generate_item_from_xml_node(xml_node, sub_item_node):
+        sub_items_nodes = xml_node.findall(sub_item_node)
+        sub_items = []
+        if sub_items_nodes is not None:
+            for sub_race in sub_items_nodes:
+                sub_items.append(GeneratedItem(sub_race.get("Name")))
+        return GeneratedItem(xml_node.get("Name"), sub_items)
