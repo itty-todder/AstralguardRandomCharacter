c56806fe2519d27447fb05ac4a78bf6d11956cc7
itty.todder <itty.todder@gmail.com>
2022-03-22 23:47:39 +0200

    Added classes support

--- CharacterGenerator/GeneratorProvider.py
+++ CharacterGenerator/GeneratorProvider.py
@@ -7,6 +7,10 @@ RACES_XML_NODE = "Races"
 RACE_XML_NODE = "Race"
 SUB_RACE_XML_NODE = "Subrace"
 
+CLASSES_XML_NODE = "Classes"
+CLASS_XML_NODE = "Class"
+SUB_CLASS_XML_NODE = "Subclass"
+
 class GeneratorProvider:
 
     def __init__(self, generator_source):
@@ -15,15 +19,32 @@ class GeneratorProvider:
             raise ValueError('Generator source file provided does not exist.')
         generator_xml_file_root = ET.parse(generator_source).getroot()
         self.__set_races(generator_xml_file_root)
+        self.__set_classes(generator_xml_file_root)
 
+#TODO: This is duplicate
     def __set_races(self, generator_xml_file_root):
         races_root_node = generator_xml_file_root.find(RACES_XML_NODE)
+        if races_root_node is None:
+            print("Races node does not exist.")
+            return
         races_nodes = races_root_node.findall(RACE_XML_NODE)
         self.races = []
         for race in races_nodes:
             generated_race = self.__generate_item_from_xml_node(race, SUB_RACE_XML_NODE)
 
             self.races.append(generated_race)
+
+    def __set_classes(self, generator_xml_file_root):
+        races_root_node = generator_xml_file_root.find(CLASSES_XML_NODE)
+        if races_root_node is None:
+            print("Classes node does not exist.")
+            return
+        races_nodes = races_root_node.findall(CLASS_XML_NODE)
+        self.classes = []
+        for race in races_nodes:
+            generated_race = self.__generate_item_from_xml_node(race, SUB_CLASS_XML_NODE)
+
+            self.classes.append(generated_race)
 
     @staticmethod
     def __generate_item_from_xml_node(xml_node, sub_item_node):
--- Test/test_GeneratorProvider.py
+++ Test/test_GeneratorProvider.py
@@ -9,7 +9,17 @@ class TestGeneratorProvider(unittest.TestCase):
         self.assertEqual(1, len(generator.races), "Wrong number of races found, xml only contains 1")
         self.assertEqual(generator.races[0].value, "TestRace")
 
-    def test_subraces_works(self):
+    def test_generate_loads_subraces(self):
         generator = GeneratorProvider(".\\TestFiles\\OnlyRaces.xml")
         self.assertEqual(generator.races[0].value, "TestRace")
         self.assertEqual(4, len(generator.races[0].subtypes), "Number of subraces is wrong")
+
+    def test_generator_loads_classes(self):
+        generator = GeneratorProvider(".\\TestFiles\\OnlyClass.xml")
+        self.assertEqual(1, len(generator.classes), "Wrong number of classes found, xml only contains 1")
+        self.assertEqual(generator.classes[0].value, "TestClass")
+
+    def test_generate_loads_subraces(self):
+        generator = GeneratorProvider(".\\TestFiles\\OnlyClass.xml")
+        self.assertEqual(generator.classes[0].value, "TestClass")
+        self.assertEqual(5, len(generator.classes[0].subtypes), "Number of subraces is wrong")
