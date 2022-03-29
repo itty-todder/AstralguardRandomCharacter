30bf172b3058938c4bd7bd2f40138d96e7f0dc3f
itty.todder <itty.todder@gmail.com>
2022-03-29 22:04:10 +0300

    Added dummy main

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-
+from CharacterGenerator.GeneratorProvider import GeneratorProvider
 
 if __name__ == "__main__":
-    pass
+    generator = GeneratorProvider(r".\GeneratorGeneral.xml")
+    (generated_class, generated_subclass), (race, subrace) = generator.generate_astral_guard()
+
+    print("Class:", generated_class.value)
+    print("\tSubclass:", generated_subclass.value)
+    print("Race:", race.value)
+    if subrace:
+        print("\tSubrace:", subrace.value)
